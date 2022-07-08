2d3b0a8b732739117d6cf2f62a44316ad08ada99
lyf703331869 <yliu52@nyit.edu>
2022-07-07 23:25:42 -0400

    Add getOneThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,5 +8,19 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  getOneThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No thought found by that id!" });
+          return;
+        }
+        res.status(200).json(userData);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
 };
 module.exports = thoughtController;
