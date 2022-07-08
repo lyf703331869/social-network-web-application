2c71034526e99f8c8b73f0955d6684b2dd740282
lyf703331869 <yliu52@nyit.edu>
2022-07-08 01:50:15 -0400

    fix the bug

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(routes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
