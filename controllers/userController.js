283e0127b4f8ce0b283a1d36194da74059a183ef
lyf703331869 <yliu52@nyit.edu>
2022-07-07 22:54:11 -0400

    Add get one user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,32 @@ const { User } = require("../models");
 const userController = {
   getAllUsers(req, res) {
     User.find({})
-      .sort({ _id: -1 })
+      .select("-__v")
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
+      });
+  },
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .populate({
+        path: "friends",
+      })
+      .populate({
+        path: "thoughts",
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
       });
   },
 };
